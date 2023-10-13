84bdcc6128072ce75a59c9ed2a28371c2309b2a0
Marco Ollivier <marcopollivier@gmail.com>
2023-10-13 07:51:56 -0300

    Configure project to TS

--- module-federation-projeto-integrado/webpack.config.js
+++ module-federation-projeto-integrado/webpack.config.js
@@ -1,13 +1,30 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+// webpack.config.js
+const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
 
 module.exports = {
-    plugins: [
-        new ModuleFederationPlugin({
-            name: "main",
-            remotes: {
-                // Local para definicao dos microfronts 
-            },
-            shared: ["react", "react-dom"],
-        })
-    ]
-}
+  entry: "./src/index.tsx",
+  output: {
+    publicPath: "http://localhost:3000/",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "main",
+      remotes: {
+        // Aqui você definirá os seus microfrontends
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+};
