d7e2574951bea418025dc4814c9b9762497cb3b0
Marco Ollivier <marcopollivier@gmail.com>
2023-10-14 19:49:07 -0300

    create base microfront

---
+++ microfrontend-projeto-integrado-main/src/Button.tsx
@@ -0,0 +1,8 @@
+// src/Button.tsx
+import React from "react";
+
+const Button: React.FC = () => {
+  return <button>Microfrontend 1 Button</button>;
+};
+
+export default Button;
--- module-federation-projeto-integrado/src/App.tsx
+++ module-federation-projeto-integrado/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx (projeto principal)
+import React, { useEffect } from "react";
 
-function App() {
+const App: React.FC = () => {
+  useEffect(() => {
+    const loadMicrofrontend = async () => {
+      const { Button } = window.microfrontendProjetoIntegradoMain
+      // Agora você pode usar o componente Button do microfrontend
+    };
+
+    loadMicrofrontend();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Projeto Principal</h1>
+      {/* Renderize o componente do microfrontend aqui */}
     </div>
   );
-}
+};
 
 export default App;
